397b5223a043da3336a142ac65f862d2e117941a
DMDZ99 <ldhdo430@naver.com>
2025-12-08 15:16:03 +0900

    [Feat]UI캔버스정리및추가

--- Assets/02. Script/Orbit/RocketOrbitController.cs
+++ Assets/02. Script/Orbit/RocketOrbitController.cs
@@ -10,5 +10,21 @@ public class RocketOrbitController : MonoBehaviour
     private bool isLaunched = false;
     private bool isOrbit = false;
 
+    // 클릭시 isLaunched = true, isOrbit = false
+    public void LaunchRocket()
+    {
+        isLaunched = true;
+        isOrbit = false;
+    }
+
+    // 궤도 들어갈시 isLaunched = false, isOrbit = true
+    public void EnterOrbit()
+    {
+        isLaunched = false;
+        isOrbit = true;
+    }
+
+    // 궤도 회전 시 로켓 -> 원접선에 수평되게 로켓이. 회전
+
 
 }
