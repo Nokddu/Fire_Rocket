c62bc90ad2dcd0f434fc81c8d230c84d020210c8
DMDZ99 <ldhdo430@naver.com>
2025-12-08 13:15:02 +0900

    [Feat]궤도회전스크립트

---
+++ Assets/02. Script/Orbit/RocketOrbitController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketOrbitController : MonoBehaviour
+{
+    [Header("±Àµµ ¿Ãµø")]
+    public float orbitSpeed = 90f;
+
+    private bool isLaunched = false;
+    private bool isOrbit = false;
+
+
+}
